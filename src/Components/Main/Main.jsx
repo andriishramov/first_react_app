181b0e082a83653b26b706bc1c60becb322b0273
Andrii <shramov12@gmail.com>
2023-05-13 19:42:31 +0200

    bug fixing

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import "./Main.scss";
+import "./Main.css";
 import Section1 from "./Section1/Section1";
 import Section2 from "./Section2/Section2";
 import {Route, Routes} from "react-router-dom";
